5c34b15319570cee1412864b7fb1d5e5a195dd34
a-m-i-t-a-v-a <140612284+a-m-i-t-a-v-a@users.noreply.github.com>
2024-07-10 20:02:28 +0530

    Add to cart and remove from cart

--- src/components/SingleProducts.js
+++ src/components/SingleProducts.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { Button, Card } from 'react-bootstrap'
+import Rating from './Rating'
+import { CartState } from '../context/Context'
 
 const SingleProducts = ({prod}) => {
+
+  const {state:{cart},dispatch}=CartState()
+
   return (
-    <div>
-       {prod.title}
+    <div className='products'>
+       <Card>
+          <Card.Img variant="top" src={prod.thumbnail} alt={prod.title}/>
+          <Card.Body>
+             <Card.Title>{prod.title}</Card.Title>
+             <Card.Subtitle style={{paddingBottom:10}}>
+                <span>$ {prod.price}</span>
+                {prod.rating > 4.5 ? (
+                    <div>Top Quality</div>
+                ):(
+                    <div>Good quality</div>
+                )}
+                <Rating rating={prod.rating}/>
+             </Card.Subtitle>
+             {cart.some(p=>p.id===prod.id) ? (
+                <Button variant="danger"  
+                    onClick={()=>{
+                        dispatch({
+                            type:'remove_from_cart',
+                            payload:prod
+                        })
+                }}>Remove from cart</Button>
+             ):(
+                <Button onClick={()=>{
+                    dispatch({
+                        type:'add_to_cart',
+                        payload:prod
+                    })
+                }}>Add to Cart</Button>
+             )}
+             
+          </Card.Body>
+       </Card>
     </div>
   )
 }
--- src/context/Reducers.js
+++ src/context/Reducers.js
@@ -2,6 +2,10 @@ export const cartReducer=(state,action)=>{
     switch(action.type){
         case 'set_products':
             return {...state,products:action.payload}
+        case 'add_to_cart':
+            return {...state,cart:[...state.cart,{...action.payload,qty:1}]}
+        case 'remove_from_cart':
+            return {...state,cart:state.cart.filter(c=>c.id!==action.payload.id)}
         default:
             return state
     }
