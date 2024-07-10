d62658bb83a1963f38be3729b0b2330916491069
a-m-i-t-a-v-a <140612284+a-m-i-t-a-v-a@users.noreply.github.com>
2024-07-10 16:57:53 +0530

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Badge, Container, Dropdown, FormControl, Nav, Navbar } from 'react-bootstrap'
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <Navbar bg="dark" variant="dark" style={{height:80}}> 
+       <Container>
+          <Navbar.Brand>
+             <Link to='/'>Shopping cart</Link>
+          </Navbar.Brand>
+          <Navbar.Text className='search'>
+             <FormControl 
+                style={{ width:500 }}
+                placeholder='Search a product'
+                className='m-auto'
+            />
+          </Navbar.Text>
+          <Nav>
+            <Dropdown>
+                <Dropdown.Toggle variant='success'>
+                    <AiOutlineShoppingCart color='white' fontSize="25px"/>
+                    <Badge>{10}</Badge>
+                </Dropdown.Toggle>
+                <Dropdown.Menu style={{minWidth:370}}>
+                    <span style={{padding:10}}>Cart is empty</span>
+                </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
+       </Container>
+    </Navbar>
+  )
+}
+
+export default Header
---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShoppingCart = () => {
+  return (
+    <div>
+      Cart
+    </div>
+  )
+}
+
+export default ShoppingCart
